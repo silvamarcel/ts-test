7261049ed9a79e3f644edd524ce1c955cefbcef2
Marcel Silva <marcel.silva@aurion.com>
2022-01-18 22:18:49 +1000

    implementation of getArnieQuotes

--- src/get-arnie-quotes.spec.ts
+++ src/get-arnie-quotes.spec.ts
@@ -1,4 +1,4 @@
-import { getArnieQuotes } from './get-arnie-quotes';
+import { getResult, getArnieQuotes, getKeyNameByStatus } from './get-arnie-quotes';
 
 const urls = [
   'http://www.smokeballdev.com/arnie0',
@@ -9,14 +9,14 @@ const urls = [
 
 test('expect no throws', () => {
   expect.assertions(1);
-  expect(async () => await getArnieQuotes(urls)).not.toThrow(); 
+  expect(async () => await getArnieQuotes(urls)).not.toThrow();
 });
 
 test('responses to be correct', async () => {
   expect.assertions(5);
 
   const results = await getArnieQuotes(urls);
-  
+
   expect(results.length).toBe(4);
 
   expect(results[0]).toEqual({ 'Arnie Quote': 'Get to the chopper' });
@@ -31,7 +31,35 @@ test('code to be executed in less than 400ms', async () => {
   const startTime = process.hrtime();
   await getArnieQuotes(urls);
   const [ seconds, nanos ] = process.hrtime(startTime);
-  
+
   expect(seconds).toBe(0);
   expect(nanos / 1000 / 1000).toBeLessThan(400);
+});
+
+describe('getKeyNameByStatus()', () => {
+  test('return Arnie Quote when status is 200', async () => {
+    expect(getKeyNameByStatus(200)).toEqual('Arnie Quote');
+  });
+
+  test('return FAILURE when status is NOT 200', async () => {
+    expect(getKeyNameByStatus(500)).toEqual('FAILURE');
+  });
+});
+
+describe('getResult()', () => {
+  test('return Arnie Quote key with any message', async () => {
+    const response = await getResult(Promise.resolve({
+      status: 200,
+      body: JSON.stringify({ message: 'any message' }),
+    }));
+    expect(response).toEqual({ 'Arnie Quote': 'any message' });
+  });
+
+  test('return FAILURE key with any message', async () => {
+    const response = await getResult(Promise.resolve({
+      status: 400,
+      body: JSON.stringify({ message: 'any message' }),
+    }));
+    expect(response).toEqual({ 'FAILURE': 'any message' });
+  });
 });
--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,24 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type TResult = Record<string, string>;
+
+export const getKeyNameByStatus = (status: number): string => {
+  return status === 200 ? 'Arnie Quote' : 'FAILURE';
+};
+
+export const getResult = async (response: Promise<{ body: string; status: number }>) => {
+  const result = await response;
+  const key = getKeyNameByStatus(result.status);
+  const value = JSON.parse(result.body)['message'];
+  return {
+    [key]: value,
+  }
+}
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+  const results: Promise<TResult>[] = [];
+  for (const url of urls) {
+    results.push(getResult(httpGet(url)));
+  }
+  return Promise.all(results);
 };
